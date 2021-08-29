8840c392ddba92e5440f904413a3b0ae7efef92a
Connor Hutchinson <BusinessHutch@gmail.com>
2021-08-29 11:53:38 -0400

    Initial Commit

---
+++ Weather/WeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherApp.swift
+//  Weather
+//
+//  Created by Connor Hutchinson on 8/29/21.
+//
+
+import SwiftUI
+
+@main
+struct WeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
